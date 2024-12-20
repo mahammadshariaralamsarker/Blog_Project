498687a05f3b434db3e067ddb90d3b040b6c1ddd
mahammadshariaralam <mahammadshariaralam@gmail.com>
2024-12-20 14:14:58 +0600

    20-12-2024 First commit

--- src/app/modules/blogs/blogs.model.ts
+++ src/app/modules/blogs/blogs.model.ts
@@ -1,1 +1,25 @@
+import { model, Schema } from "mongoose";
+import { TBlog } from "./blogs.interface";
 
+const blogSchema = new Schema <TBlog>(
+  {
+    title:{
+      type:String,
+      required:true
+    },
+    content:{
+      type:String,
+      required:true  
+    },
+    author:{
+      type:Schema.Types.ObjectId,
+      required:true
+    }
+  },
+  {
+    timestamps:true,
+    versionKey:false
+  }
+)
+
+export const Blogs = model<TBlog>('Blog', blogSchema)
