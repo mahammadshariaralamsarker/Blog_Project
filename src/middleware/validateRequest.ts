ab602a4b41325dfbbbd5c6dc813f058ed32dee69
mahammadshariaralam <mahammadshariaralam@gmail.com>
2024-12-21 16:45:51 +0600

    add validate request

---
+++ src/middleware/validateRequest.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from 'express';
+import { AnyZodObject } from 'zod';
+import { catchAsync } from '../app/utils/catchAsync';
+// import { catchAsync } from '../utils/CatchAsync';
+
+export const validateRequest = (Schema: AnyZodObject) => {
+  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    await Schema.parseAsync({
+      body: req.body,
+    });
+    next();
+  });
+};
