c6cd96ed7a7175e691f0d01a875716a215beea1a
mahammadshariaralam <mahammadshariaralam@gmail.com>
2024-12-22 21:51:06 +0600

    modify

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,31 @@
-import express, { Application } from 'express';
-import cors from 'cors';
-import router from './app/routes';
-import { globalErrorHandler } from './app/middleware/globalErrorHandler';
-import { notFound } from './app/middleware/notFound';
+import express, { Application, Request, Response } from "express";
+import cors from 'cors'
+import router from "./app/routes";
+import { globalErrorHandler } from "./app/middleware/globalErrorHandler";
+import { notFound } from "./app/middleware/notFound";
+ 
+const app: Application = express()
 
-const app: Application = express();
-app.use(express.json());
-app.use(cors());
+// parserrs
+app.use(express.json())
+app.use(cors())
 
-app.use('/api', router);
-app.use(globalErrorHandler);
-app.use(notFound);
+// root
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello Server");
+});
+
+// all router
+app.use('/api', router)
+
+// global error handel 
+app.use(globalErrorHandler)
+
+// not found page
+app.use(notFound)
+
 export default app;
+
+
+
+ 
